e7fc841ad3faef078ca032be4d93d65b41b8d364
MinhIsNotFine <minhminh2532013@gmail.com>
2025-12-15 22:45:55 +0700

    Delete js directory

--- js/auth.js
+++
@@ -1,3 +0,0 @@
-function login(){ auth.signInWithEmailAndPassword(email.value,pass.value); }
-function signup(){ auth.createUserWithEmailAndPassword(email.value,pass.value); }
-auth.onAuthStateChanged(u=>{ if(u) console.log('logged'); });
